293d44aec3379a905f49435e1468e2c124f334d4
Scotty Weeks <scott.weeks@gmail.com>
2024-03-13 07:49:44 -0400

    feat(build) emit type declarations in vite build

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [tsconfigPaths()],
+  plugins: [tsconfigPaths(), dts({ insertTypesEntry: true })],
   build: {
     lib: {
       entry: 'src/index.ts',
