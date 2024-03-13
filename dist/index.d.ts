9f5509871629755c67a3806f0e0bf3efb15af1c3
Scotty Weeks <scott.weeks@gmail.com>
2024-03-13 08:28:19 -0400

    feat(build) bundling types

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,13 +1,24 @@
-import { type StoreApi } from 'zustand';
-import { type NamedSet } from 'zustand/middleware';
-type FirstSetStateArg<T> = Parameters<StoreApi<T>['setState']>[0];
-export type SetState<T> = (partial: FirstSetStateArg<T>, name?: string) => void;
-export type GetState<T> = StoreApi<T>['getState'];
-type StateObject = Record<string, any>;
-type SliceKey<S extends StateObject> = {
-    [K in keyof S]: S[K] extends S ? K : never;
-}[keyof StateObject];
-interface SliceConfig {
+import { NamedSet } from 'zustand/middleware';
+import { StoreApi } from 'zustand';
+
+export declare type CallLog = undefined | string | {
+    type: string;
+    payload: any;
+};
+
+export declare type CreateSlice<Slice extends StateObject, AppState extends StateObject = StateObject> = (set: SetState<Slice>, get: GetState<Slice>, store: StoreApi<AppState>) => Slice;
+
+declare type FirstSetStateArg<T> = Parameters<StoreApi<T>['setState']>[0];
+
+export declare type GetState<T> = StoreApi<T>['getState'];
+
+export declare type SetState<T> = (partial: FirstSetStateArg<T>, name?: string) => void;
+
+declare const slice: <S extends StateObject>(set: NamedSet<S>, get: GetState<S>, store: StoreApi<S>, config?: SliceConfig) => <K extends SliceKey<S>>(k: K, init: CreateSlice<S[K], S>) => S[K];
+export default slice;
+export { slice }
+
+declare interface SliceConfig {
     /**
      *  1. `false`: calls to `set` will not be logged
      *  2. `type`: logs the `<slice name>.<type>`
@@ -17,12 +28,15 @@ interface SliceConfig {
      */
     logCalls?: false | 'type' | 'all';
 }
+
 export declare const sliceGet: <S extends StateObject, K extends SliceKey<S>>(get: GetState<S>, key: K) => GetState<S[K]>;
-export type CallLog = undefined | string | {
-    type: string;
-    payload: any;
-};
+
+declare type SliceKey<S extends StateObject> = {
+    [K in keyof S]: S[K] extends S ? K : never;
+}[keyof StateObject];
+
 export declare const sliceSet: <S extends StateObject, K extends SliceKey<S>>(set: NamedSet<S>, key: K, config: SliceConfig) => SetState<S[K]>;
-export type CreateSlice<Slice extends StateObject, AppState extends StateObject = StateObject> = (set: SetState<Slice>, get: GetState<Slice>, store: StoreApi<AppState>) => Slice;
-export declare const slice: <S extends StateObject>(set: NamedSet<S>, get: GetState<S>, store: StoreApi<S>, config?: SliceConfig) => <K extends SliceKey<S>>(k: K, init: CreateSlice<S[K], S>) => S[K];
-export default slice;
+
+declare type StateObject = Record<string, any>;
+
+export { }
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,14 @@ import dts from 'vite-plugin-dts';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [tsconfigPaths(), dts({ insertTypesEntry: true })],
+  plugins: [
+    tsconfigPaths(),
+    dts({
+      staticImport: true,
+      rollupTypes: true,
+
+    })
+  ],
   build: {
     lib: {
       entry: 'src/index.ts',
